1da614e65c3c9382ac5c85afe5d565396e450938
bazycristi21 <45794107+bazycristi21@users.noreply.github.com>
2019-11-18 13:41:59 +0200

    Update Homework lab 4.cpp

--- Lab Robotics/Homework lab 4.cpp
+++ Lab Robotics/Homework lab 4.cpp
@@ -15,6 +15,8 @@ const int noOfDisplays = 4;
 const int noOfDigits = 10;
 const int pinX = A0;
 const int pinY = A1;
+const int pinSW = 3;
+
 int numbers[4];
 int xValue,yValue;
 int lastDebounceTime = 0;
@@ -28,7 +30,9 @@ int dpState = LOW;
 int currentNumber = 0;
 unsigned long delayCounting = 50; // incrementing interval
 unsigned long lastIncreasing = 0;
-
+int swPressed = 0;
+int readingSW,swValue=5;
+int swBack=1;
 
 // segments array, similar to before
 int segments[segSize] = {
@@ -57,14 +61,18 @@ void displayNumber(byte digit, byte decimalPoint) {
     digitalWrite(segments[i], digitMatrix[digit][i]);
   }
   // write the decimalPoint to DP pin
-  digitalWrite(segments[segSize - 1], decimalPoint);
+  for(int i=0;i<4;i++)
+    if(i==digit)
+      digitalWrite(pinDP,LOW);
   }
 // activate the display no. received as param
 void showDigit(int num) {
   for (int i = 0; i < noOfDisplays; i++) {
     digitalWrite(digits[i], HIGH);  
+    digitalWrite(pinDP, HIGH);
   }
   digitalWrite(digits[num], LOW);
+  
 }
 
 void setup() {
@@ -79,43 +87,61 @@ pinMode(digits[i], OUTPUT);
 }
 pinMode(pinX,INPUT);
 pinMode(pinY,INPUT);
+pinMode(pinSW,INPUT);
 Serial.begin(9600);
 }
 
 
 void loop() {
-    for(int i=0;i<4;i++)
+    digitalWrite(pinDP,LOW);
+    for(int i = 0; i < 4; i++)
     {
       showDigit(i);
-      displayNumber(numbers[i], HIGH);
+      displayNumber(numbers[i], LOW);
       delay(5);
     // increase this delay to see multiplexing in action. At about 100 it
     }   
     readingX = analogRead(pinY);
     xValue = readingX;
-    Serial.println(xValue);
-    if(xValue > 700 && digit > 0 && xAxisTilted == false)
+    readingSW = digitalRead(pinSW);
+    Serial.println(readingSW);
+    if(readingSW == 0 && swPressed == 0 && swBack == 1)
     {
-         Serial.println("a intrat");
+      swPressed = 1;
+      swBack = 0;
+    }
+    else if(swBack == 1 && swPressed == 1 && readingSW == 0)
+    {
+      swPressed = 0;
+      swBack = 0;  
+    }
+    if(readingSW > 0)
+    {
+      swBack = 1;
+    }
+    
+    
+    if(xValue > 700 && digit > 0 && xAxisTilted == false && swPressed == 0)
+    {
          digit--; // move to next display
          xAxisTilted = true;
     }
-    if(xValue < 300 && digit <4 && xAxisTilted == false)
-     {
-        Serial.println("a intrat");
+    if(xValue < 300 && digit < 4 && xAxisTilted == false && swPressed == 0)
+    {
         digit++;
-        xAxisTilted =true;
-     }    
+        xAxisTilted = true;
+    }    
     if(xValue > 300 && xValue <700)
       xAxisTilted = false;
       
     readingY = analogRead(pinX);
     yValue = readingY;
-    if(yValue > 700 and yAxisTilted ==false  and numbers[digit]<9){
+    if(yValue > 700 and yAxisTilted == false and numbers[digit] < 9 && swPressed == 1){
+        
         numbers[digit]++;
         yAxisTilted = true;
     }
-    if(yValue < 300 and yAxisTilted == false and numbers[digit] >0)
+    if(yValue < 300 and yAxisTilted == false and numbers[digit] > 0 && swPressed == 1)
     {
         numbers[digit]--;
         yAxisTilted = true;
@@ -124,10 +150,6 @@ void loop() {
     {
       yAxisTilted = false;
     }
-    
-
-      
-  
     // increment the number
    }
   
